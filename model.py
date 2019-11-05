4dbc3ff682d501c1ea4919ee5c6b2cd3ea0fe356
Susanna Lyons <SusannaRLyons@gmail.com>
2019-11-05 06:00:47 +0000

    Working on the data model. WIP.

--- model.py
+++ model.py
@@ -1,7 +1,4 @@
 
-from datetime import datetime
-import enum
-import flask 
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -14,16 +11,15 @@ class User(UserMixin, db.Model):
 
     user_id = db.Column(db.Integer, 
                         primary_key=True,
-                        autoincrement=True,
+                        autoincrement=True
                         )
-    fname = db.Column(db.String(50), nullable=False,)
-    lname = db.Column(db.String(50), nullable=False,)
-    email = db.Column(db.String(100), nullable=False, unique=True,)
-    '''
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    fname = db.Column(db.String(50), nullable=False)
+    lname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
     password_hash = db.Column(db.String(50), nullable=False,)
-    '''
-    created_at = db.Column(db.DateTime, db.default=datetime.now)
-    updated_at = db.Column(db.DateTime, db.onupdate=datetime.now)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -31,12 +27,17 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    db.session.add()
-    db.session.commit()
-
     def __repr__(self):
-    """Show user information."""
+    """Show helpful user information."""
         return f"<User user_id={self.user_id} fname={self.fname} lname={self.lanme} email={self.email}"
+
+
+# Association Table with the log_id's and bird_id's.
+bird_field_log_association_table = db.Table('birds_field_logs', db.Base.metadata,
+    db.Column('log_id', db.Integer, db.ForeignKey('field_logs.log_id')),
+    db.Column('bird_id', db.Integer, db.ForeignKey('birds.bird_id'))
+)
+
 
 class Bird(db.Model): 
     """Bird and species information."""
@@ -45,16 +46,15 @@ class Bird(db.Model):
 
     bird_id = db.Column(db.Integer, 
                         primary_key=True,
-                        autoincrement=True,
+                        autoincrement=True
                         )
-    '''
-    species = db.Column(db.String(75), nullable=False, unique=True,)..... Use Enum?
-    '''
-    created_at = db.Column(db.DateTime, db.default=datetime.now)
-    updated_at = db.Column(db.DateTime, db.onupdate=datetime.now)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    db.session.add()
-    db.session.commit()
+    species = db.Column(db.String(75), nullable=False, unique=True)
+
+    field_logs = db.relationship('Field_Log', secondary=bird_field_log_association_table, backref='birds') 
+
 
 class Field_Log(db.Model): 
     """Log book to track birds seen and related information."""
@@ -63,49 +63,29 @@ class Field_Log(db.Model):
 
     log_id = db.Column(db.Integer, 
                        primary_key=True,
-                       autoincrement=True,
+                       autoincrement=True
                        )
-    '''
-    date = db.Column(db.Date)......
-    time = db.Column(db.Time)......
-    '''
-    location = db.Column(db.String(100), nullable=False,)
-    weather = db.Column(db.String(100),)
-    habitat = db.Column(db.String(100),)
-    equipment = db.Column(db.String(100),)
-    notes = db.Column(db.Text,)
-    created_at = db.Column(db.DateTime, db.default=datetime.now)
-    updated_at = db.Column(db.DateTime, db.onupdate=datetime.now)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    # When creating data for date and time, remember to use datetime.date/time.
+    date = db.Column(db.Date)
+    time = db.Column(db.Time)
+    location = db.Column(db.String(100), nullable=False)
+    weather = db.Column(db.String(100))
+    habitat = db.Column(db.String(100))
+    equipment = db.Column(db.String(100))
+    notes = db.Column(db.Text)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
 
 
-    db.session.add()
-    db.session.commit()
-
-
+    user = db.relationship('User', backref='field_logs')
+    
     def __repr__(self):
-    """Show field log information."""
-        return f"<Feild_Log log_id={self.log_id} date={self.date} location={self.location}"
+    """Show helpful field log information."""
+        return f"<Field_Log log_id={self.log_id} date={self.date} location={self.location}"
 
-class Log_Birds(db.Model):
-    """List of all birds seen on a particular day."""
 
-    __tablename__ = "birds_seen"
-
-    log_birds_id = db.Column(db.Integer, 
-                             primary_key=True,
-                             autoincrement=True,
-                             )
-
-    log_id = db.Column(db.Integer, db.ForeignKey('field_logs.log_id'))
-    bird_id = db.Column(db.Integer, db.ForeignKey('birds.brid_id'))
-
-    db.session.add()
-    db.session.commit()
-
-    def __repr__(self):
-    """Show birds in curent log."""
-        return f"<Birds birds_seen={self.log_birds_id} current_log={self.log_id}"
 
 
 """
