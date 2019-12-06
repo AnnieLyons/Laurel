8b8684ba0cf5072aac744798e6324961e356d2a3
Susanna Lyons <susannarlyons@gmail.com>
2019-12-05 20:47:56 -0800

    Site now flexes to fit viewport (phone, tablet, laptop).

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from jinja2 import StrictUndefined
+from  jinja2 import StrictUndefined
 from flask import Flask, render_template, request, flash, redirect, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from model import connect_to_db, db, User, Field_Log, Bird, bird_field_log_association_table
